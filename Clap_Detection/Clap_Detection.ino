1a85b75f3c95e3dae2a8701c68e878481ba3b481
Surya Narayanan <118114664+Soorya-Narayan@users.noreply.github.com>
2024-07-13 14:14:31 +0530

    Add files via upload

---
+++ Clap_Detection/Clap_Detection.ino
@@ -0,0 +1,23 @@
+int sensor = 2;
+int led = 13;
+boolean is_on = false;
+
+void setup(){
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop(){
+  int data = digitalRead(sensor);
+
+  if(data == 1){
+    if(is_on == true){
+      digitalWrite(led, LOW);
+      is_on = false;
+    }
+    else{
+      digitalWrite(led, HIGH);
+      is_on = true;
+    }
+  }
+}
